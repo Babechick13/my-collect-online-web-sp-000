c4b550655416594ca68deef193e6c118655c4b7b
Jessica Natovich <jessicanatovich@yahoo.com>
2021-01-20 23:12:31 +0000

    Automatically backed up by Learn

--- spec/my_collect_spec.rb
+++ spec/my_collect_spec.rb
@@ -1,5 +1,7 @@
+end 
 describe "my_collect" do
   let(:languages) { ['ruby', 'javascript', 'python', 'objective-c'] }
+ end 
   let(:students) { ['Tim Jones', 'Tom Smith', 'Sophie Johnson', 'Antoin Miller'] }
   it "can handle an empty collection" do
 end 
